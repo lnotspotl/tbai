fa007b05c0dcf1662064946f7ff32031f743c9cd
lnotspotl <lnotspotl@gmail.com>
2025-06-18 20:14:05 -0400

    Update virtual methods

--- tbai_core/include/tbai_core/control/StateSubscriber.hpp
+++ tbai_core/include/tbai_core/control/StateSubscriber.hpp
@@ -10,10 +10,10 @@ class StateSubscriber {
    public:
     virtual ~StateSubscriber() = default;
 
-    virtual void waitTillInitialized() const = 0;
-    virtual vector_t &getLatestRbdState() const = 0;
-    virtual scalar_t getLatestRbdStamp() const = 0;
-    virtual std::vector<bool> &getContactFlags() const = 0;
+    virtual void waitTillInitialized() = 0;
+    virtual const vector_t &getLatestRbdState() const = 0;
+    virtual const scalar_t getLatestRbdStamp() const = 0;
+    virtual const std::vector<bool> &getContactFlags() const = 0;
 };
 
 }  // namespace tbai
