fc746a0138908a6dbdc733b85e665e284d6d78a4
lnotspotl <lnotspotl@gmail.com>
2025-07-11 02:24:26 -0400

    add a few more buttons

--- tbai_deploy_go2/include/tbai_deploy_go2/Go2JoystickInterface.hpp
+++ tbai_deploy_go2/include/tbai_deploy_go2/Go2JoystickInterface.hpp
@@ -35,14 +35,21 @@ public:
     void SetGamepadDeadZone(float deadzone) {gamepad.dead_zone = deadzone;}
     void setGamepadSmooth(float smooth) {gamepad.smooth = smooth;}
     void MessageHandler(const void *message);
-    virtual void onPressA() {TBAI_LOG_DEBUG(logger_, "A pressed");}
-    virtual void onPressB() {TBAI_LOG_DEBUG(logger_, "B pressed");}
-    virtual void onPressX() {TBAI_LOG_DEBUG(logger_, "X pressed");}
-    virtual void onPressY() {TBAI_LOG_DEBUG(logger_, "Y pressed");}
-    virtual void inReleaseA() {TBAI_LOG_DEBUG(logger_, "A released");}
-    virtual void inReleaseB() {TBAI_LOG_DEBUG(logger_, "B released");}
-    virtual void inReleaseX() {TBAI_LOG_DEBUG(logger_, "X released");}
-    virtual void inReleaseY() {TBAI_LOG_DEBUG(logger_, "Y released");}
+    virtual void onPressA() {TBAI_LOG_WARN(logger_, "A pressed");}
+    virtual void onPressB() {TBAI_LOG_WARN(logger_, "B pressed");}
+    virtual void onPressX() {TBAI_LOG_WARN(logger_, "X pressed");}
+    virtual void onPressY() {TBAI_LOG_WARN(logger_, "Y pressed");}
+    virtual void inReleaseA() {TBAI_LOG_WARN(logger_, "A released");}
+    virtual void inReleaseB() {TBAI_LOG_WARN(logger_, "B released");}
+    virtual void inReleaseX() {TBAI_LOG_WARN(logger_, "X released");}
+    virtual void inReleaseY() {TBAI_LOG_WARN(logger_, "Y released");}
+    virtual void onPressStart() {TBAI_LOG_WARN(logger_, "Start pressed");}
+    virtual void onPressSelect() {TBAI_LOG_WARN(logger_, "Select pressed");}
+    virtual void onPressL1() {TBAI_LOG_WARN(logger_, "L1 pressed");}
+    virtual void onPressR1() {TBAI_LOG_WARN(logger_, "R1 pressed");}
+    virtual void onPressL2() {TBAI_LOG_WARN(logger_, "L2 pressed");}
+    virtual void onPressR2() {TBAI_LOG_WARN(logger_, "R2 pressed");}
+    virtual void onPressF1() {TBAI_LOG_WARN(logger_, "F1 pressed");}
     void Step();
     void Start();
 
