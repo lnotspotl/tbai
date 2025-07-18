02ef0ec363fe8027809622ca4a2beca84da9f306
lnotspotl <lnotspotl@gmail.com>
2025-07-18 16:23:15 -0400

    improve logging

--- tbai_core/src/Logging.cpp
+++ tbai_core/src/Logging.cpp
@@ -1,15 +1,21 @@
 #include "tbai_core/Logging.hpp"
 
+#include <filesystem>
+#include <fstream>
 #include <mutex>
 
 #include "spdlog/spdlog.h"
 #include <spdlog/async.h>
+#include <tbai_core/Files.hpp>
 
 namespace tbai {
 
 static std::shared_ptr<spdlog::details::thread_pool> globalThreadPool;
 
 std::shared_ptr<spdlog::logger> getLogger(const std::string &name) {
+    static std::mutex getLoggerMutex;
+    std::lock_guard<std::mutex> lock(getLoggerMutex);
+
     auto logLevel = tbai::getEnvAsChecked<std::string>(
         "TBAI_LOG_LEVEL", {"trace", "debug", "info", "warn", "error", "fatal"}, true, "info");
     auto logFolder = tbai::getEnvAs<std::string>("TBAI_LOG_FOLDER", true, "");
@@ -18,18 +24,32 @@ std::shared_ptr<spdlog::logger> getLogger(const std::string &name) {
 
     std::vector<spdlog::sink_ptr> sinks;
     if (logToConsole) {
-        sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        consoleSink->set_pattern("[%H:%M:%S.%e] [%^%l%$] [%n] %v");
+        sinks.push_back(consoleSink);
     }
 
     if (!logFolder.empty()) {
-        sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFolder + "/" + name + ".logs", true));
+        static bool logFolderScript = false;
+        if (!logFolderScript) {
+            std::filesystem::create_directories(logFolder);
+            auto fileLock = tbai::FileLock::lock(logFolder + "/combine_logs.sh", true);
+            std::ofstream combineFile(logFolder + "/combine_logs.sh");
+            combineFile << "sort -t']' -k1,1 ./*.logs > combined.txt" << std::endl;
+            combineFile.close();
+            logFolderScript = true;
+        }
+
+        auto logFile = logFolder + "/" + name + ".logs";
+        if (logFolderScript) {
+            auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFile, true);
+            fileSink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%n] %v");
+            sinks.push_back(fileSink);
+        }
     }
 
     std::shared_ptr<spdlog::logger> logger;
     if (useAsyncLogging) {
-        static std::mutex globalThreadPoolMutex;
-        std::lock_guard<std::mutex> lock(globalThreadPoolMutex);
-
         if (!globalThreadPool) {
             globalThreadPool = std::make_shared<spdlog::details::thread_pool>(8192, 2);
         }
@@ -41,7 +61,6 @@ std::shared_ptr<spdlog::logger> getLogger(const std::string &name) {
         logger = std::make_shared<spdlog::logger>(name, sinks.begin(), sinks.end());
     }
     logger->set_level(spdlog::level::from_str(logLevel));
-    logger->set_pattern("[%H:%M:%S.%e] [%^%l%$] [%n] %v");
     return logger;
 }
 
