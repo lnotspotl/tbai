9bc31e7b4690bfb1361d325536eeb168378871f0
lnotspotl <lnotspotl@gmail.com>
2026-01-06 19:32:55 +0100

    add franka mpc

---
+++ tbai_mpc/src/franka_mpc/FactoryFunctions.cpp
@@ -0,0 +1,107 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#include "tbai_mpc/franka_mpc/FactoryFunctions.h"
+
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/multibody/joint/joint-composite.hpp>
+#include <pinocchio/multibody/model.hpp>
+
+#include <boost/property_tree/info_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include <ocs2_core/misc/LoadData.h>
+#include <ocs2_pinocchio_interface/urdf.h>
+
+namespace ocs2 {
+namespace mobile_manipulator {
+
+PinocchioInterface createPinocchioInterface(const std::string& robotUrdfPath, const ManipulatorModelType& type) {
+  switch (type) {
+    case ManipulatorModelType::DefaultManipulator:
+      return getPinocchioInterfaceFromUrdfFile(robotUrdfPath);
+    default:
+      throw std::invalid_argument("Invalid manipulator model type provided.");
+  }
+}
+
+PinocchioInterface createPinocchioInterface(const std::string& robotUrdfPath, const ManipulatorModelType& type,
+                                            const std::vector<std::string>& jointNames) {
+  using joint_pair_t = std::pair<const std::string, std::shared_ptr<::urdf::Joint>>;
+
+  const auto urdfTree = ::urdf::parseURDFFile(robotUrdfPath);
+  ::urdf::ModelInterfaceSharedPtr newModel = std::make_shared<::urdf::ModelInterface>(*urdfTree);
+  for (joint_pair_t& jointPair : newModel->joints_) {
+    if (std::find(jointNames.begin(), jointNames.end(), jointPair.first) != jointNames.end()) {
+      jointPair.second->type = urdf::Joint::FIXED;
+    }
+  }
+
+  switch (type) {
+    case ManipulatorModelType::DefaultManipulator:
+      return getPinocchioInterfaceFromUrdfModel(newModel);
+    default:
+      throw std::invalid_argument("Invalid manipulator model type provided.");
+  }
+}
+
+ManipulatorModelInfo createManipulatorModelInfo(const PinocchioInterface& interface, const ManipulatorModelType& type,
+                                                const std::string& baseFrame, const std::string& eeFrame) {
+  const auto& model = interface.getModel();
+
+  ManipulatorModelInfo info;
+  info.manipulatorModelType = type;
+  info.stateDim = model.nq;
+
+  switch (type) {
+    case ManipulatorModelType::DefaultManipulator:
+      info.inputDim = info.stateDim;
+      info.armDim = info.inputDim;
+      break;
+    default:
+      throw std::invalid_argument("Invalid manipulator model type provided.");
+  }
+
+  info.eeFrame = eeFrame;
+  info.baseFrame = baseFrame;
+  const auto& jointNames = model.names;
+  info.dofNames = std::vector<std::string>(jointNames.end() - info.armDim, jointNames.end());
+
+  return info;
+}
+
+ManipulatorModelType loadManipulatorType(const std::string& configFilePath, const std::string& fieldName) {
+  boost::property_tree::ptree pt;
+  boost::property_tree::read_info(configFilePath, pt);
+  const size_t type = pt.template get<size_t>(fieldName);
+  return static_cast<ManipulatorModelType>(type);
+}
+
+}  // namespace mobile_manipulator
+}  // namespace ocs2
---
+++ tbai_mpc/src/franka_mpc/MobileManipulatorPinocchioMapping.cpp
@@ -0,0 +1,76 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#include "tbai_mpc/franka_mpc/MobileManipulatorPinocchioMapping.h"
+
+namespace ocs2 {
+namespace mobile_manipulator {
+
+template <typename SCALAR>
+MobileManipulatorPinocchioMappingTpl<SCALAR>::MobileManipulatorPinocchioMappingTpl(ManipulatorModelInfo info)
+    : modelInfo_(std::move(info)) {}
+
+template <typename SCALAR>
+MobileManipulatorPinocchioMappingTpl<SCALAR>* MobileManipulatorPinocchioMappingTpl<SCALAR>::clone() const {
+  return new MobileManipulatorPinocchioMappingTpl<SCALAR>(*this);
+}
+
+template <typename SCALAR>
+auto MobileManipulatorPinocchioMappingTpl<SCALAR>::getPinocchioJointPosition(const vector_t& state) const -> vector_t {
+  return state;
+}
+
+template <typename SCALAR>
+auto MobileManipulatorPinocchioMappingTpl<SCALAR>::getPinocchioJointVelocity(const vector_t& state, const vector_t& input) const
+    -> vector_t {
+  switch (modelInfo_.manipulatorModelType) {
+    case ManipulatorModelType::DefaultManipulator:
+      return input;
+    default:
+      throw std::runtime_error("The chosen manipulator model type is not supported!");
+  }
+}
+
+template <typename SCALAR>
+auto MobileManipulatorPinocchioMappingTpl<SCALAR>::getOcs2Jacobian(const vector_t& state, const matrix_t& Jq, const matrix_t& Jv) const
+    -> std::pair<matrix_t, matrix_t> {
+  switch (modelInfo_.manipulatorModelType) {
+    case ManipulatorModelType::DefaultManipulator:
+      return {Jq, Jv};
+    default:
+      throw std::runtime_error("The chosen manipulator model type is not supported!");
+  }
+}
+
+// explicit template instantiation
+template class ocs2::mobile_manipulator::MobileManipulatorPinocchioMappingTpl<ocs2::scalar_t>;
+template class ocs2::mobile_manipulator::MobileManipulatorPinocchioMappingTpl<ocs2::ad_scalar_t>;
+
+}  // namespace mobile_manipulator
+}  // namespace ocs2
